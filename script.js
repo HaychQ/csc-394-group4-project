5123207e121c1f8d4e59e6642412cec03a633d03
BSWIERZ7 <bart601722@gmail.com>
2022-10-07 07:55:57 -0500

    added displayGames function - updates HTML

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const userInfo = {
   apiKey: "",
 };
 
+// HTML ID SELECTORS
 // User Input Textbox
 const userInput = document.getElementById("userInput");
 
@@ -14,6 +15,9 @@ const btnUserInput = document.getElementById("btn_UserInput");
 
 // Form - Contains API Key(string) given by User
 const userInputForm = document.getElementById("userInputForm");
+
+// Individual Game Selectors
+const game1 = document.getElementById("game-1");
 
 btnUserInput.addEventListener("click", function (event) {
   // PREVENTS FORM SUBMISSION
@@ -27,4 +31,27 @@ btnUserInput.addEventListener("click", function (event) {
 
   console.log("User Input: ", inputText);
   console.log("userInfo: ", userInfo);
+
+  //Call function to display games
+  displayGames();
 });
+
+/* 
+  Updates Game slot - Example/Placeholder - Game image will be displayed here
+  Updates each game using String literals
+*/
+function displayGames() {
+  for (let i = 1; i < 11; i++) {
+    document.getElementById(`game-${i}`).innerHTML = `Display${i}`;
+  }
+}
+// document.getElementById("game-1").innerHTML = "Test1";
+// document.getElementById("game-2").innerHTML = "Test2";
+// document.getElementById("game-3").innerHTML = "Test3";
+// document.getElementById("game-4").innerHTML = "Test4";
+// document.getElementById("game-5").innerHTML = "Test5";
+// document.getElementById("game-6").innerHTML = "Test6";
+// document.getElementById("game-7").innerHTML = "Test7";
+// document.getElementById("game-8").innerHTML = "Test8";
+// document.getElementById("game-9").innerHTML = "Test9";
+// document.getElementById("game-10").innerHTML = "Test10";
